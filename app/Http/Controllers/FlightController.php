0fb4af927c3fdfad9c7cdffb20f9710f6c78a612
kodjak <andrecouto10@gmail.com>
2017-03-11 01:25:09 +0000

    fix2

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -84,7 +84,7 @@ class FlightController extends Controller
         $flight->country=$inputs['country'];
         $flight->url=$inputs['url'];
         $flight->facebookshare=$inputs['facebookshare'];
-        $destinationFileName = $flight->enddate.$flight->name.'.'.$request->file('picture')->getClientOriginalExtension();
+        $destinationFileName = $request->file('picture')->getClientOriginalName();
         $request->file('picture')->move(
            public_path(), $destinationFileName);
         $flight->picture =  $destinationFileName;
@@ -157,7 +157,7 @@ class FlightController extends Controller
         $flight->country=$inputs['country'];
         $flight->url=$inputs['url'];
         $flight->facebookshare=$inputs['facebookshare'];
-        $destinationFileName = $request->file('picture')->getFilename().$request->file('picture')->getClientOriginalExtension();
+        $destinationFileName = $request->file('picture')->getClientOriginalName();
         $request->file('picture')->move(
             public_path(), $destinationFileName);
         $flight->picture =  $destinationFileName;
