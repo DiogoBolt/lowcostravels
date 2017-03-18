089bcff162b7cf6b7a920fa1d8bcd88c7a099dd9
Diogo Araujo <diogoboltaraujo@gmail.com>
2017-03-18 00:52:47 +0000

    fix layout

--- resources/views/flights/showflight.blade.php
+++ resources/views/flights/showflight.blade.php
@@ -22,7 +22,7 @@
             <div style="margin-top:10px;float:right;" > <a href="{{$flight->url}}" target="_blank"class="btn btn-primary">Book Now</a></div>
         </div>
 
-        <div style="margin-top:370px; margin-right:730px;" class="fb-share-button" a href="{{$flight->facebookshare}}" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Partilhar</a></div>
+        <div class="fb-share-button" a href="{{$flight->facebookshare}}" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">Partilhar</a></div>
 
 
         <div id="fb-root"></div>
