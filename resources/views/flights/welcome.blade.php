5d745a870901c72e2bf6695d568da0aef2bd12bf
kodjak <andrecouto10@gmail.com>
2017-03-26 10:34:52 +0100

    google ads welcome blade

--- resources/views/flights/welcome.blade.php
+++ resources/views/flights/welcome.blade.php
@@ -101,7 +101,13 @@
 
 <!-- jQuery -->
 
-
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-2892547033485231",
+        enable_page_level_ads: true
+    });
+</script>
 
     <script type="text/JavaScript" src="https://www.kayak.com/affiliate/widget.js"></script>
 <script type="text/JavaScript">
