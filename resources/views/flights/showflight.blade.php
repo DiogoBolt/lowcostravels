abe9f70f9dae8f2613e605db7a52d498add77f43
Diogo Araujo <diogoboltaraujo@gmail.com>
2017-05-27 23:35:54 +0100

    SEO

--- resources/views/flights/showflight.blade.php
+++ resources/views/flights/showflight.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="{{$flight->description}}">
     <meta name="keywords" content="{{$flight->country}},{{$flight->zone}} ,cheap flights, travel deals, air tickets, air deals, holiday, flights low cost, lowcostravels, low cost travels, last minute flights, voos baratos, viagens baratas, flights promotions, promocoes de viagens">
-    <meta name="author" content="LowCostravels">
+    <meta name="author" content="LowCostravels - Flight Deals">
     <meta name="verification" content="4e7c4154c463887d03a1d53383f8675e" />
     <meta property="og:image" content="{{URL::to('/').'/'.$flight->picture}}">
 
